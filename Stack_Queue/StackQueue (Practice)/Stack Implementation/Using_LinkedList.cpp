e2a680cea347ebdb270017e633b5f9d8cb29dc9b
Sanju-Basak <1805064@ugrad.cse.buet.ac.bd>
2021-08-04 17:18:30 +0600

    Create Using_LinkedList.cpp

---
+++ Stack_Queue/StackQueue (Practice)/Stack Implementation/Using_LinkedList.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+#include<assert.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    string str;
+    Node *next;
+};
+
+class Stack
+{
+    Node *head= NULL;
+public:
+    void push(string str)
+    {
+        Node *node= new Node();
+        node->str= str;
+        node->next= head;
+        head= node;
+    }
+    string pop()
+    {
+        if(!isEmpty())
+        {
+            string str1= head->str;
+            head= head->next;
+            return str1;
+        }
+        else
+            return "Stack is Empty!!";
+
+    }
+    string top()
+    {
+        if(!isEmpty())
+        {
+            return head->str;
+        }
+        else
+            return "Stack is Empty!!";
+    }
+
+    bool isEmpty()
+    {
+        return head==NULL;
+    }
+};
+
+int main()
+{
+   Stack stk;
+   stk.push("you!");
+   stk.push("love");
+   stk.push("I");
+   cout<< stk.pop()<< " ";
+   cout<< stk.pop()<< " ";
+   cout<< stk.pop()<< " ";
+}
+
